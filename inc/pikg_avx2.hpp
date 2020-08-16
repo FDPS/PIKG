c310afb9887f2a7726f94fb61dcfb3f479844b26
subarutaro <subarutaro@gmail.com>
2020-08-16 12:45:53 +0900

    add static to avoid multiple definition

--- inc/pikg_avx2.hpp
+++ inc/pikg_avx2.hpp
@@ -19,26 +19,26 @@ struct __m256x3{
 struct __m256x4{
   __m256 v0,v1,v2,v3;
 };
-__m256dx2 _mm256_set1_pdx2(const PIKG::F64vec2 v){
+static inline __m256dx2 _mm256_set1_pdx2(const PIKG::F64vec2 v){
   __m256dx2 ret;
   ret.v0 = _mm256_set1_pd(v.x);
   ret.v1 = _mm256_set1_pd(v.y);
   return ret;
 }
-__m256x2  _mm256_set1_psx2(const PIKG::F32vec2 v){
+static inline __m256x2  _mm256_set1_psx2(const PIKG::F32vec2 v){
   __m256x2 ret;
   ret.v0 = _mm256_set1_ps(v.x);
   ret.v1 = _mm256_set1_ps(v.y);
   return ret;
 }
-__m256dx3 _mm256_set1_pdx3(const PIKG::F64vec v){
+static inline __m256dx3 _mm256_set1_pdx3(const PIKG::F64vec v){
   __m256dx3 ret;
   ret.v0 = _mm256_set1_pd(v.x);
   ret.v1 = _mm256_set1_pd(v.y);
   ret.v2 = _mm256_set1_pd(v.z);
   return ret;
 }
-__m256x3  _mm256_set1_psx3(const PIKG::F32vec v){
+static inline __m256x3  _mm256_set1_psx3(const PIKG::F32vec v){
   __m256x3 ret;
   ret.v0 = _mm256_set1_ps(v.x);
   ret.v1 = _mm256_set1_ps(v.y);
--- inc/pikg_avx512.hpp
+++ inc/pikg_avx512.hpp
@@ -1,48 +1,48 @@
 #ifndef H_PIKG_AVX_512
 #define H_PIKG_AVX_512
 #include <immintrin.h>
-  struct __m512dx2{
+struct __m512dx2{
   __m512d v0,v1;
-  };
-  struct __m512dx3{
+};
+struct __m512dx3{
   __m512d v0,v1,v2;
-  };
-  struct __m512dx4{
+};
+struct __m512dx4{
   __m512d v0,v1,v2,v3;
-  };
-  struct __m512x2{
+};
+struct __m512x2{
   __m512 v0,v1;
-  };
-  struct __m512x3{
+};
+struct __m512x3{
   __m512 v0,v1,v2;
-  };
-  struct __m512x4{
+};
+struct __m512x4{
   __m512 v0,v1,v2,v3;
-  };
-  __m512dx2 _mm512_set1_pdx2(const PIKG::F64vec2 v){
+};
+static inline __m512dx2 _mm512_set1_pdx2(const PIKG::F64vec2 v){
   __m512dx2 ret;
   ret.v0 = _mm512_set1_pd(v.x);
   ret.v1 = _mm512_set1_pd(v.y);
   return ret;
-  }
-  __m512x2  _mm512_set1_psx2(const PIKG::F32vec2 v){
+}
+static inline __m512x2  _mm512_set1_psx2(const PIKG::F32vec2 v){
   __m512x2 ret;
   ret.v0 = _mm512_set1_ps(v.x);
   ret.v1 = _mm512_set1_ps(v.y);
   return ret;
-  }
-  __m512dx3 _mm512_set1_pdx3(const PIKG::F64vec v){
+}
+static inline __m512dx3 _mm512_set1_pdx3(const PIKG::F64vec v){
   __m512dx3 ret;
   ret.v0 = _mm512_set1_pd(v.x);
   ret.v1 = _mm512_set1_pd(v.y);
   ret.v2 = _mm512_set1_pd(v.z);
   return ret;
-  }
-  __m512x3  _mm512_set1_psx3(const PIKG::F32vec v){
+}
+static inline __m512x3  _mm512_set1_psx3(const PIKG::F32vec v){
   __m512x3 ret;
   ret.v0 = _mm512_set1_ps(v.x);
   ret.v1 = _mm512_set1_ps(v.y);
   ret.v2 = _mm512_set1_ps(v.z);
   return ret;
-  }
+}
 #endif
