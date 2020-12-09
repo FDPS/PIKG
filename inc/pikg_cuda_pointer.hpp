e950587ee85772e39f59bdb5494885584149f3d7
subarutaro <subarutaro@gmail.com>
2020-12-09 11:43:29 +0900

    modify cuda_pointer

--- inc/pikg_cuda_pointer.hpp
+++ inc/pikg_cuda_pointer.hpp
@@ -2,13 +2,14 @@
 #define PIKG_CUDA_POINTER
 
 #include <cassert>
+#include <cstdlib>
 #include <pikg_vector.hpp>
 
 namespace PIKG{
   template <typename T>
   class CUDAPointer{
   public:
-    //T* hst = nullptr;
+    T* hst = nullptr;
     T* dev = nullptr;
     U64 _size = 0;
     U64 _limit = 0;
@@ -16,28 +17,26 @@ namespace PIKG{
     ~CUDAPointer(){
       free();
     }
-    /*    
     void free_host(){
       if(hst != nullptr){
-	free(hst);
+	delete[] hst;
 	hst = nullptr;
-      } 
+      }
     }
-    */
     void free_dev(){
       if(dev != nullptr){
 	cudaFree(dev);
 	dev = nullptr;
-      } 
+      }
     }
     void free(){
-      //free_host();
-      free_dev(); 
+      free_host();
+      free_dev();
     }
 
-    void malloc(const U64 n){
-      free();
-      //hst = (T*)malloc(n*sizeof(T));
+    void allocate(const U64 n){
+      //free();
+      hst = new T[n];
       cudaMalloc((void**)&dev,n*sizeof(T));
       _size = n;
       _limit = n;
@@ -51,14 +50,25 @@ namespace PIKG{
     }
 
     void reserve(const U64 n){
-      if(size==0 || _limit < n) malloc(n);
+      if(_size==0 || _limit < n) allocate(n);
     }
 
-    void h2d(const T* hst,const U64 n = _size){
+    void h2d(const U64 n){
       cudaMemcpy(dev,hst,n*sizeof(T),cudaMemcpyHostToDevice);
     }
-    void d2h(T* hst) const {
-      cudaMemcpy(hst,dev,size*sizeof(T),cudaMemcpyDeviceToHost);
+    void d2h(const U64 n) const {
+      cudaMemcpy(hst,dev,n*sizeof(T),cudaMemcpyDeviceToHost);
+    }
+
+    const T& operator[](const int index) const {
+      return hst[index];
+    }
+    T& operator[](const int index){
+      return hst[index];
+    }
+
+    operator T*(){
+      return dev;
     }
   };
 };
