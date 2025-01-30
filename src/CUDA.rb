147aa5898a20e0fdc5327ade41d8fb0c4127da0a
subarutaro <subarutaro@gmail.com>
2025-01-30 11:41:27 +0900

    fix type

--- src/CUDA.rb
+++ src/CUDA.rb
@@ -15,7 +15,7 @@ class Kernelprogram
     code += "#include \"pikg_vector.hpp\"\n"
 
     code += "__device__ float  table(float  tab[],int i){ return tab[i]; }\n"
-    code += "__device__ double table(double tab[],in5 i){ return tab[i]; }\n"
+    code += "__device__ double table(double tab[],int i){ return tab[i]; }\n"
 
     fvars = generate_force_related_map(@statements)
     # GPU class definition
